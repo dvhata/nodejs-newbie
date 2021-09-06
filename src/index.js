5665159391a66cc36c5997bce17f4af3487c55e0
dvhata <thaohanh233@gmail.com>
2021-09-06 17:20:05 +0700

    Installation done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const port = 3000; // default
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '/resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(port,() => console.log('Example app listening on at http:/localhost:' + port));
