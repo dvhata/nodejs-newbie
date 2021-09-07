3f0b24b839b3b8095434e48c7ba1ab665fd0bda3
dvhata <thaohanh233@gmail.com>
2021-09-08 01:46:12 +0700

    scss install

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app = express();
 const handlebars = require('express-handlebars');
 const port = 3000; // default
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/resources/views'));
